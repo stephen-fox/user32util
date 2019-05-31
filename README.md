27966ac9002abd3dae32c32caf010b29354dfed2
Stephen Fox <stephen.j.fox.jr@gmail.com>
2019-05-30 20:27:58 -0400

    Added some really basic documentation.

---
+++ README.md
@@ -0,0 +1,2 @@
+# winuserio
+A rudimentary Windows user IO helper library written in Go.
---
+++ doc.go
@@ -0,0 +1,2 @@
+// Package winuserio provides a rudimentary Windows user IO helper library.
+package winuserio
