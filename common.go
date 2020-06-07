b4a62335a88b11c19a1c03b3200504addcdaf100
Stephen Fox <stephen.j.fox.jr@gmail.com>
2020-06-07 13:07:06 -0400

    Moved hook API code into 'common.go'.

--- common.go
+++ common.go
@@ -1,5 +1,50 @@
 package winuserio
 
+import "golang.org/x/sys/windows"
+
+func newHooksWinApi() (*hooksWinApi, error) {
+	user32, err := windows.LoadDLL(user32DllName)
+	if err != nil {
+		return nil, err
+	}
+
+	set, err := user32.FindProc(setWindowsHookExAName)
+	if err != nil {
+		return nil, err
+	}
+
+	call, err := user32.FindProc(callNextHookExName)
+	if err != nil {
+		return nil, err
+	}
+
+	unhook, err := user32.FindProc(unhookWindowsHookExName)
+	if err != nil {
+		return nil, err
+	}
+
+	getMessageW, err := user32.FindProc(getMessageWName)
+	if err != nil {
+		return nil, err
+	}
+
+	return &hooksWinApi{
+		user32:              user32,
+		setWindowsHookExA:   set,
+		callNextHookEx:      call,
+		unhookWindowsHookEx: unhook,
+		getMessageW:         getMessageW,
+	}, nil
+}
+
+type hooksWinApi struct {
+	user32              *windows.DLL
+	setWindowsHookExA   *windows.Proc
+	callNextHookEx      *windows.Proc
+	unhookWindowsHookEx *windows.Proc
+	getMessageW         *windows.Proc
+}
+
 type hookSetupResult struct {
 	handle uintptr
 	err    error
--- kb.go
+++ kb.go
@@ -73,14 +73,6 @@ func (o KbDllHookStruct) VirtualKeyCode() byte {
 	return byte(o.VkCode)
 }
 
-type hooksWinApi struct {
-	user32              *windows.DLL
-	setWindowsHookExA   *windows.Proc
-	callNextHookEx      *windows.Proc
-	unhookWindowsHookEx *windows.Proc
-	getMessageW         *windows.Proc
-}
-
 func NewLowLevelKeyboardListener(fn OnLowLevelKeyboardEventFunc) (*LowLevelKeyboardEventListener, error) {
 	hooksWinApi, err := newHooksWinApi()
 	if err != nil {
@@ -135,40 +127,5 @@ func NewLowLevelKeyboardListener(fn OnLowLevelKeyboardEventFunc) (*LowLevelKeybo
 		hookHandle:  result.handle,
 		fn:          fn,
 		done:        done,
-	}, nil
-}
-
-func newHooksWinApi() (*hooksWinApi, error) {
-	user32, err := windows.LoadDLL(user32DllName)
-	if err != nil {
-		return nil, err
-	}
-
-	set, err := user32.FindProc(setWindowsHookExAName)
-	if err != nil {
-		return nil, err
-	}
-
-	call, err := user32.FindProc(callNextHookExName)
-	if err != nil {
-		return nil, err
-	}
-
-	unhook, err := user32.FindProc(unhookWindowsHookExName)
-	if err != nil {
-		return nil, err
-	}
-
-	getMessageW, err := user32.FindProc(getMessageWName)
-	if err != nil {
-		return nil, err
-	}
-
-	return &hooksWinApi{
-		user32:              user32,
-		setWindowsHookExA:   set,
-		callNextHookEx:      call,
-		unhookWindowsHookEx: unhook,
-		getMessageW:         getMessageW,
 	}, nil
 }
